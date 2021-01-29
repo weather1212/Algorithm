738df4c9fb584add45d296fd7a1cfb8d8bead59f
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-01-30 01:35:32 +0900

    Create Baek_1000.java
    문자열
    A+B (B5)

---
+++ 코딩테스트/20210129/Baek_1000.java
@@ -0,0 +1,17 @@
+package _20210129;
+
+import java.util.Scanner;
+
+public class Baek_1000 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a + b);
+	}
+
+}
+// A+B (B5)
