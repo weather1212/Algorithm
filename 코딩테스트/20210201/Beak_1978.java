ca6a3d6d8d5418ad769cef32278cd4a0d8c6ef4d
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-02-02 02:10:33 +0900

    Create Beak_1978.java
    소수찾기 (S4)

---
+++ 코딩테스트/20210201/Beak_1978.java
@@ -0,0 +1,40 @@
+package _20210201;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beak_1978 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		br.readLine();
+		int count = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		while (st.hasMoreTokens()) {
+			boolean isPrime = true;
+
+			int num = Integer.parseInt(st.nextToken());
+
+			if (num == 1) {
+				continue;
+			}
+			for (int i = 2; i <= Math.sqrt(num); i++) {
+				if (num % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
+// 소수찾기 (S4)
